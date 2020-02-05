823ac542c6c137fda56aff365b03ccaab3bb0039
Ksaveras Šakys <xawiers@gmail.com>
2020-02-05 22:03:53 +0000

    Initial commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ksaveras\CircuitBreakerBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration.
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('ksaveras_circuit_breaker');
+
+        $treeBuilder->getRootNode()
+            ->children()
+                ->arrayNode('twitter')
+                    ->children()
+                        ->integerNode('client_id')->end()
+                        ->scalarNode('client_secret')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ KsaverasCircuitBreakerBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ksaveras\CircuitBreakerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class KsaverasCircuitBreakerBundle.
+ */
+class KsaverasCircuitBreakerBundle extends Bundle
+{
+}
