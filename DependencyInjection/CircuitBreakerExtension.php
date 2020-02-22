54df488119ca68327fd03bb26e838809e3944ed4
Ksaveras Šakys <xawiers@gmail.com>
2020-02-22 18:06:16 +0000

    Create services based on configuration

--- DependencyInjection/CircuitBreakerExtension.php
+++ DependencyInjection/CircuitBreakerExtension.php
@@ -4,19 +4,28 @@ declare(strict_types=1);
 
 namespace Ksaveras\CircuitBreakerBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
-class CircuitBreakerExtension extends Extension
+class CircuitBreakerExtension extends ConfigurableExtension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function loadInternal(array $config, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        foreach ($config['circuit_breakers'] as $name => $serviceConfig) {
+            $id = sprintf('%s.%s', $this->getAlias(), $name);
 
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+            $storageService = new Reference($serviceConfig['storage']);
+
+            $arguments = [
+                $name,
+                $storageService,
+                $serviceConfig['reset_period']
+            ];
+
+            $container->register($id, 'Ksaveras\CircuitBreaker\CircuitBreaker')
+                ->setPublic(false)
+                ->setArguments($arguments);
+        }
     }
 }
