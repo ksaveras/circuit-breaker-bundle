61ffd3504b8367c954ca63aa94e44e9819526004
Ksaveras Sakys <xawiers@gmail.com>
2020-04-30 08:50:37 +0100

    CS fix

--- DependencyInjection/CircuitBreakerExtension.php
+++ DependencyInjection/CircuitBreakerExtension.php
@@ -7,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Ksaveras\CircuitBreakerBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
