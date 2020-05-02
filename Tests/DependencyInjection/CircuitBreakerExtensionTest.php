532599c7f37bf95e1fb2b4c169bd05f754a4ad21
Ksaveras Sakys <xawiers@gmail.com>
2020-05-02 10:25:41 +0100

    Test invalid storage exception

--- Tests/DependencyInjection/CircuitBreakerExtensionTest.php
+++ Tests/DependencyInjection/CircuitBreakerExtensionTest.php
@@ -13,7 +13,9 @@ use Ksaveras\CircuitBreaker\CircuitBreaker;
 use Ksaveras\CircuitBreaker\Storage\PhpArray;
 use Ksaveras\CircuitBreakerBundle\DependencyInjection\CircuitBreakerExtension;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class CircuitBreakerExtensionTest extends TestCase
 {
@@ -30,6 +32,7 @@ class CircuitBreakerExtensionTest extends TestCase
         ];
 
         $container = new ContainerBuilder();
+        $container->register('event_dispatcher', EventDispatcherInterface::class);
         $container->register('storage_service', PhpArray::class);
 
         $extension = new CircuitBreakerExtension();
@@ -43,9 +46,29 @@ class CircuitBreakerExtensionTest extends TestCase
         $this->assertEquals('demo', $definition->getArgument(0));
         $this->assertEquals(60, $definition->getArgument(2));
 
-        $expectedMethodCalls = [
-            ['setFailureThreshold', [5]],
+        $methodCalls = $definition->getMethodCalls();
+        $this->assertContains(['setFailureThreshold', [5]], $methodCalls);
+    }
+
+    public function testInvalidStorageService(): void
+    {
+        $config = [
+            [
+                'circuit_breakers' => [
+                    'demo' => [
+                        'storage' => 'file_storage',
+                    ],
+                ],
+            ],
         ];
-        $this->assertEquals($expectedMethodCalls, $definition->getMethodCalls());
+
+        $container = new ContainerBuilder();
+        $container->register('file_storage', \stdClass::class);
+
+        $this->expectException(InvalidConfigurationException::class);
+        $this->expectExceptionMessage('Invalid "file_storage" storage service. Service must implement "Ksaveras\CircuitBreaker\Storage\StorageInterface" interface.');
+
+        $extension = new CircuitBreakerExtension();
+        $extension->load($config, $container);
     }
 }
