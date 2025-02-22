ea100f779735a56b9f1a23941fd547b4c2bb61c9
Ksaveras Sakys <xawiers@gmail.com>
2025-02-22 18:09:13 +0200

    chore: Update PHP version to 8.3 and apply minor code style improvements

--- src/DependencyInjection/Factory/Storage/AbstractStorageFactory.php
+++ src/DependencyInjection/Factory/Storage/AbstractStorageFactory.php
@@ -7,12 +7,13 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Ksaveras\CircuitBreakerBundle\DependencyInjection\Factory\Storage;
 
 abstract class AbstractStorageFactory implements StorageFactoryInterface
 {
     protected function serviceId(string $name): string
     {
-        return sprintf('ksaveras_circuit_breaker.storage.%s', $name);
+        return \sprintf('ksaveras_circuit_breaker.storage.%s', $name);
     }
 }
